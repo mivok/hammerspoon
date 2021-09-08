8ccd01f851cef9fdfe878be0893253b057419ea6
Mark Harrison <mark@mivok.net>
2021-09-07 20:58:51 -0400

    Add a zoom layer, switch layers to be named layers

--- streamdeck.lua
+++ streamdeck.lua
@@ -12,7 +12,7 @@ end
 
 local layers = {
   -- Layer 1 - Main layer
-  {
+  default = {
     -- Row 1
     { },
     {
@@ -29,7 +29,7 @@ local layers = {
     },
     {
       image = streamdeck_imageFromText("2", "Layer"),
-      press_callback = streamdeck_changeLayerCallback(2)
+      press_callback = streamdeck_changeLayerCallback('layer2')
     },
     -- Row 2
     {
@@ -45,7 +45,10 @@ local layers = {
       press_callback = hotkey_audio_laptop,
     },
     {},
-    {},
+    {
+      image = streamdeck_imageWithLabel("zoom.png", "Zoom", {scale=0.6}),
+      press_callback = streamdeck_changeLayerCallback('zoom')
+    },
     -- Row 3
     {
       image = streamdeck_imageWithLabel('shush.png', 'Shush', {
@@ -60,7 +63,7 @@ local layers = {
   },
 
   -- Layer 2
-  {
+  layer2 = {
     -- Row 1
     {},
     {},
@@ -68,7 +71,7 @@ local layers = {
     {},
     {
       image = streamdeck_imageFromText("<", "Back"),
-      press_callback = streamdeck_changeLayerCallback(1)
+      press_callback = streamdeck_changeLayerCallback('default')
     },
     -- Row 2
     {},
@@ -79,7 +82,34 @@ local layers = {
     -- Row 3
     {
       -- Shush
-      passthrough = true
+      passthrough = 'default'
+    },
+    {},
+    {},
+    {},
+    {},
+  },
+  -- Layer 3 - Zoom Layer
+  zoom = {
+    -- Row 1
+    {},
+    {},
+    {},
+    {},
+    {},
+    -- Row 2
+    {},
+    {},
+    {},
+    {},
+    {
+      image = streamdeck_imageFromText("<", "Back"),
+      press_callback = streamdeck_changeLayerCallback('default')
+    },
+    -- Row 3
+    {
+      -- Shush
+      passthrough = 'default'
     },
     {},
     {},
@@ -87,7 +117,7 @@ local layers = {
     {},
   },
   -- Layer N - Blank template layer
-  {
+  blank = {
     -- Row 1
     {},
     {},
@@ -112,11 +142,10 @@ local layers = {
 function streamdeck_buttonCallback(sd, button_number, pressed)
   -- If a button has the passthrough attribute set, keep going up a layer
   -- until we find a non-passthrough button
-  local buttonLayer = currentLayer
-  repeat
-    button = layers[buttonLayer][button_number]
-    buttonLayer = buttonLayer - 1
-  until buttonLayer == 0 or not button.passthrough
+  local button = layers[currentLayer][button_number]
+  if button.passthrough then
+    button = layers[button.passthrough][button_number]
+  end
 
   if pressed and button.press_callback then
     button.press_callback()
@@ -131,11 +160,9 @@ function streamdeck_changeLayer(layer)
   for idx, button in ipairs(layers[currentLayer]) do
 
     -- Deal with passthrough layers
-    local buttonLayer = currentLayer
-    repeat
-      button = layers[buttonLayer][idx]
-      buttonLayer = buttonLayer - 1
-    until buttonLayer == 0 or not button.passthrough
+    if button.passthrough then
+      button = layers[button.passthrough][idx]
+    end
 
     if button and button.image then
       streamdeckDevice:setButtonImage(idx, button.image)
@@ -152,7 +179,7 @@ function streamdeck_init(connected, sd)
     streamdeckDevice = nil
   end
 
-  streamdeck_changeLayer(1)
+  streamdeck_changeLayer('default')
   sd:buttonCallback(streamdeck_buttonCallback)
 end
 
